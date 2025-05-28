74243563aa6039fe15eb465116bb987adcdc7c70
nguyenyou <tunt081295@gmail.com>
2025-05-28 09:22:58 +0700

    🚢 ship it

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <button className="trns-o focus:outline-light cursor-pointer hover:bg-gray-100 active:bg-gray-200 border border-gray-300 rounded text-[13px] h-[32px] px-3">Click me</button>
+    </div>
+  );
+}
+
+export default App;
